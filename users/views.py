1c544e4595dc94ed7f04dae8553b28c7c5a9a2e1
Ammar Nammour <Ammar.nammour@digitalcareerinstitute.org>
2024-05-31 10:47:30 +0200

    1

--- users/views.py
+++ users/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-
+from django.views.generic.edit import CreateView
+from .forms import LoginForm
 # Create your views here.
+
+class UserLogin(CreateView):
+    form_class=LoginForm
+    template_name='users/login.html'
+    # model=
+    # success_url=
