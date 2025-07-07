f7249ccbd77d829a23d0f33015fe13e867a50991
Mhd Ammar Nammour <144100697+Ammar6nam@users.noreply.github.com>
2025-07-08 00:30:28 +0200

    Update settings.py

--- socialproject/settings.py
+++ socialproject/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ywxo-jwxq+f(gg1d+o@u@o1-lw48yz_rwb@235u*(b71lroz6v'
+SECRET_KEY = '************************************************'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
